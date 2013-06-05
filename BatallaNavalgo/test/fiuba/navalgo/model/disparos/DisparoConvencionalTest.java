61201691f85fa361136740f2c59ff5cd4ead289f
briglia.antonella@gmail.com <briglia.antonella@gmail.com@c7809e7b-64bf-1911-2ad3-b94c0bcf2527>
2013-06-05 21:51:42 +0000

    Paquete y prueba de disparo convencional

---
+++ BatallaNavalgo/test/fiuba/navalgo/model/disparos/DisparoConvencionalTest.java
@@ -0,0 +1,17 @@
+package fiuba.navalgo.model.disparos;
+
+import junit.framework.TestCase;
+
+public class DisparoConvencionalTest extends TestCase {
+	public void testCrearUnDisparoConvencional(){
+		DisparoConvencional disparo = new DisparoConvencional();
+		assertNotNull(disparo);
+	}
+	
+	public void testCrearUnDisparoEstaListoParaSerDisparado(){
+		DisparoConvencional disparo = new DisparoConvencional();
+		assertTrue(disparo.listoParaDisparar());
+	}
+	
+
+}
