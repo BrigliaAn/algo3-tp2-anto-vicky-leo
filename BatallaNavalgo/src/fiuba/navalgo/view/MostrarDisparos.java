19ac6ca7e83477726a682c38e1abce20e3cb868b
galdames.leonardo@gmail.com <galdames.leonardo@gmail.com@c7809e7b-64bf-1911-2ad3-b94c0bcf2527>
2013-07-03 16:41:23 +0000

    mostrarDisparos esta en una clase fuera de la principal

---
+++ BatallaNavalgo/src/fiuba/navalgo/view/MostrarDisparos.java
@@ -0,0 +1,52 @@
+package fiuba.navalgo.view;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import fiuba.navalgo.model.disparos.Disparo;
+import fiuba.navalgo.model.disparos.MinaSubmarinaDoble;
+import fiuba.navalgo.model.disparos.MinaSubmarinaPorContacto;
+import fiuba.navalgo.model.disparos.MinaSubmarinaPuntual;
+import fiuba.navalgo.model.disparos.MinaSubmarinaTriple;
+
+public class MostrarDisparos {
+	
+public void mostrarDisparos(JButtonID tablero[][],Disparo disparo) throws IOException{
+		
+
+		int columna = disparo.devolverCasillaPrincipal().getPosicion().getColumna();
+		int fila = disparo.devolverCasillaPrincipal().getPosicion().getFila();
+			
+		if(disparo instanceof MinaSubmarinaPuntual){		
+			BufferedImage wpuntual= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/puntual.png"));
+			ImageIcon puntual = new ImageIcon(wpuntual);
+			tablero[fila][columna].setIcon(new ImageIcon(puntual.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
+				
+		}
+		if(disparo instanceof MinaSubmarinaDoble){
+			BufferedImage wdoble= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/doble.png"));
+			ImageIcon doble = new ImageIcon(wdoble);
+			tablero[fila][columna].setIcon(new ImageIcon(doble.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
+				
+		}
+		
+		if(disparo instanceof MinaSubmarinaTriple){
+			BufferedImage wtriple= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/triple.png"));
+			ImageIcon triple = new ImageIcon(wtriple);
+			tablero[fila][columna].setIcon(new ImageIcon(triple.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
+				
+		}
+		if(disparo instanceof MinaSubmarinaPorContacto){
+			BufferedImage wcontacto= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/contacto.png"));
+			ImageIcon contacto = new ImageIcon(wcontacto);
+			tablero[fila][columna].setIcon(new ImageIcon(contacto.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
+				
+		}
+		
+	}
+
+}
--- BatallaNavalgo/src/fiuba/navalgo/view/VentanaPrincipal.java
+++ BatallaNavalgo/src/fiuba/navalgo/view/VentanaPrincipal.java
@@ -269,9 +269,10 @@ public class VentanaPrincipal extends JFrame {
 					
 				}	
 				ArrayList<Disparo> disparos = control.getDisparos();
+				MostrarDisparos tipoDeDisparosParaMostrar = new MostrarDisparos();
 				for(Disparo disparo: disparos){
 					try {
-						mostrarDisparos(mat,disparo);
+						 tipoDeDisparosParaMostrar.mostrarDisparos(mat,disparo);
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -351,9 +352,10 @@ public class VentanaPrincipal extends JFrame {
 							
 						}	
 						ArrayList<Disparo> disparos = control.getDisparos();
+						MostrarDisparos tipoDeDisparosParaMostrar = new MostrarDisparos();
 						for(Disparo disparo: disparos){
 							try {
-								mostrarDisparos(mat,disparo);
+								tipoDeDisparosParaMostrar.mostrarDisparos(mat,disparo);
 							} catch (IOException e) {
 								e.printStackTrace();
 							}
@@ -659,38 +661,7 @@ public class VentanaPrincipal extends JFrame {
 		
 	}
 	
-	public void mostrarDisparos(JButtonID tablero[][],Disparo disparo) throws IOException{
-		
-		int columna = disparo.devolverCasillaPrincipal().getPosicion().getColumna();
-		int fila = disparo.devolverCasillaPrincipal().getPosicion().getFila();
-		
-		if(disparo instanceof MinaSubmarinaPuntual){		
-			BufferedImage wpuntual= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/puntual.png"));
-			ImageIcon puntual = new ImageIcon(wpuntual);
-			tablero[fila][columna].setIcon(new ImageIcon(puntual.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
-				
-		}
-		if(disparo instanceof MinaSubmarinaDoble){
-			BufferedImage wdoble= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/doble.png"));
-			ImageIcon doble = new ImageIcon(wdoble);
-			tablero[fila][columna].setIcon(new ImageIcon(doble.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
-				
-		}
-		
-		if(disparo instanceof MinaSubmarinaTriple){
-			BufferedImage wtriple= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/triple.png"));
-			ImageIcon triple = new ImageIcon(wtriple);
-			tablero[fila][columna].setIcon(new ImageIcon(triple.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
-				
-		}
-		if(disparo instanceof MinaSubmarinaPorContacto){
-			BufferedImage wcontacto= ImageIO.read(this.getClass().getResource("/Imagenes/MinasSubmarinas/contacto.png"));
-			ImageIcon contacto = new ImageIcon(wcontacto);
-			tablero[fila][columna].setIcon(new ImageIcon(contacto.getImage().getScaledInstance(35,30,Image.SCALE_SMOOTH)));
-				
-		}
-		
-	}
+	
 
 	public void limpiarTablero(JButtonID tablero[][]) throws IOException{
 		for (int i=0 ; i<10 ; i++){
