8cda3fc5478fcf172ea053e59baf294b1c094974
vickyperezbustos@gmail.com <vickyperezbustos@gmail.com@c7809e7b-64bf-1911-2ad3-b94c0bcf2527>
2013-07-13 15:01:50 +0000

    cambie tamanio de ventana fin del juego

--- BatallaNavalgo/src/fiuba/navalgo/view/FinJuego.java
+++ BatallaNavalgo/src/fiuba/navalgo/view/FinJuego.java
@@ -32,26 +32,26 @@ public class FinJuego extends JFrame {
 		getContentPane().setBackground(Color.CYAN);
 		
 		getContentPane().setForeground(new Color(0, 0, 0));
-		setBounds(300, 300, 355, 236);
+		setBounds(300, 300, 441, 282);
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("FIN DEL JUEGO! ");
 		lblNewLabel.setForeground(Color.BLACK);
 		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setFont(new Font("Tw Cen MT", Font.BOLD, 35));
-		lblNewLabel.setBounds(41, 24, 254, 79);
+		lblNewLabel.setBounds(77, 24, 322, 79);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblTuPuntajeFinal = new JLabel("Tu puntaje final es:");
 		lblTuPuntajeFinal.setForeground(Color.BLACK);
 		lblTuPuntajeFinal.setFont(new Font("Tw Cen MT", Font.PLAIN, 21));
-		lblTuPuntajeFinal.setBounds(27, 114, 182, 45);
+		lblTuPuntajeFinal.setBounds(44, 129, 216, 45);
 		getContentPane().add(lblTuPuntajeFinal);
 		
 		lblpuntajeFinal = new JLabel("");
 		lblpuntajeFinal.setForeground(Color.RED);
 		lblpuntajeFinal.setFont(new Font("Tw Cen MT", Font.BOLD, 22));
-		lblpuntajeFinal.setBounds(205, 114, 117, 45);
+		lblpuntajeFinal.setBounds(261, 129, 117, 45);
 		getContentPane().add(lblpuntajeFinal);
 		
 		
